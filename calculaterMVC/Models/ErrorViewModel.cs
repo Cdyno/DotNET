1e41083ca06c81ff94606ea393f45b6464db1ac8
mohamed <ola.badreldeen.mohamed@gmail.com>
2023-05-04 10:32:58 +0300

    Bulding a basic calculater following MVC structure

---
+++ calculaterMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace calculaterMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
